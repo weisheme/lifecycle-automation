8e83321d6f99dd8c2e22ff7611c6d4ce584851ff
Christian Dupuis <cd@atomist.com>
2017-10-17 14:34:20 -0700

    fix footer renderer to not fail on empty messages

--- src/lifecycle/rendering/FooterNodeRenderer.ts
+++ src/lifecycle/rendering/FooterNodeRenderer.ts
@@ -16,31 +16,33 @@ export class FooterNodeRenderer implements NodeRenderer<any> {
     }
 
     public render(node: any, actions: Action[], msg: SlackMessage, context: RendererContext): Promise<SlackMessage> {
-        const attachment = msg.attachments[msg.attachments.length - 1];
-        if (!attachment.ts) {
-            const date = Math.floor(new Date().getTime() / 1000);
-            // attachment.footer = `<!date^${date}^Updated {date_short_pretty} at {time_secs}|${date}>`;
-            attachment.ts = date;
-        }
-
-        // Render feedback url in the footer
-        const feedbackUrl =
-            `https://atomist.typeform.com/to/yvnyOj?message_id=${base64.encode(context.context.invocationId)}`;
-        const feedbackFooter = `${url(feedbackUrl, "Feedback?")}`;
-        if (attachment.footer != null) {
-            const footer = `${attachment.footer} | ${feedbackFooter}`;
-            if (footer.length <= 300) {
-                attachment.footer = footer;
+        if (msg && msg.attachments && msg.attachments.length > 0) {
+            const attachment = msg.attachments[msg.attachments.length - 1];
+            if (!attachment.ts) {
+                const date = Math.floor(new Date().getTime() / 1000);
+                // attachment.footer = `<!date^${date}^Updated {date_short_pretty} at {time_secs}|${date}>`;
+                attachment.ts = date;
             }
-        } else {
-            attachment.footer = feedbackFooter;
-        }
 
-        // Make sure markdown is enabled for the footer
-        if (attachment.mrkdwn_in != null) {
-            attachment.mrkdwn_in.push("footer");
-        } else {
-            attachment.mrkdwn_in = ["footer"];
+            // Render feedback url in the footer
+            const feedbackUrl =
+                `https://atomist.typeform.com/to/yvnyOj?message_id=${base64.encode(context.context.invocationId)}`;
+            const feedbackFooter = `${url(feedbackUrl, "Feedback?")}`;
+            if (attachment.footer != null) {
+                const footer = `${attachment.footer} | ${feedbackFooter}`;
+                if (footer.length <= 300) {
+                    attachment.footer = footer;
+                }
+            } else {
+                attachment.footer = feedbackFooter;
+            }
+
+            // Make sure markdown is enabled for the footer
+            if (attachment.mrkdwn_in != null) {
+                attachment.mrkdwn_in.push("footer");
+            } else {
+                attachment.mrkdwn_in = ["footer"];
+            }
         }
         return Promise.resolve(msg);
     }
