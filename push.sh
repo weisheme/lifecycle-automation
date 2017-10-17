c068fd0262074ee468f0b1b6cc05cc217fb842a4
Christian Dupuis <cd@atomist.com>
2017-10-17 15:40:21 -0700

    add compile step to CF push script

--- push.sh
+++ push.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+npm run lint:fix && npm run compile
+
 cf target -s production
 
 cf delete lifecycle-automation-old -f
@@ -10,12 +12,12 @@ cf push -f manifest.yml lifecycle-automation-new
 cf rename lifecycle-automation lifecycle-automation-old
 cf rename lifecycle-automation-new lifecycle-automation
 
-cf stop lifecycle-automation-old
-
 cf unmap-route lifecycle-automation cfapps.io --hostname lifecycle-automation-new
 cf map-route lifecycle-automation cfapps.io --hostname lifecycle-automation
-
-# cf unmap-route lifecycle-automation-old atomist.io --hostname lifecycle
 cf map-route lifecycle-automation atomist.io --hostname lifecycle
+
+sleep 10
+
+cf stop lifecycle-automation-old
 
 cf apps
