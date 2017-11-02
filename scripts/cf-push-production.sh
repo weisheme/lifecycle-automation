746417e75859d69e06ef7995ae7375aba2047372
Christian Dupuis <cd@atomist.com>
2017-11-02 11:56:19 +0100

    add CF deployment to build

--- scripts/cf-push-production.sh
+++ scripts/cf-push-production.sh
@@ -8,14 +8,14 @@ cf target -s production
 
 cf delete lifecycle-automation-old -f
 
-cf push -f manifest.yml lifecycle-automation-new
+cf push -f manifest-production.yml lifecycle-automation-new
 
 cf rename lifecycle-automation lifecycle-automation-old
 cf rename lifecycle-automation-new lifecycle-automation
 
 cf unmap-route lifecycle-automation cfapps.io --hostname lifecycle-automation-new
 cf map-route lifecycle-automation cfapps.io --hostname lifecycle-automation
-cf map-route lifecycle-automation atomist.io --hostname lifecycle
+# cf map-route lifecycle-automation atomist.io --hostname lifecycle
 
 sleep 10
 
