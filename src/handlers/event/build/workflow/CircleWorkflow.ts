aa117bd811f80ba1e3fedf53726a72e77b81b785
Clay McCoy <clay@atomist.com>
2017-11-06 09:57:12 -0600

    logic for converting Circle workflow events into our flat stage abstraction and render it into a chart

---
+++ src/handlers/event/build/workflow/CircleWorkflow.ts
@@ -0,0 +1,105 @@
+import * as _ from "lodash";
+import {WorkflowStage} from "./WorkflowStage";
+import {BuildStatus} from "../../../../typings/types";
+import * as yaml from "js-yaml"
+
+export class CircleWorkflow {
+    teamId: string
+    id: string
+    builds: Build[]
+    name: string
+    provider: string
+    config: string
+}
+
+export interface Build {
+    teamId: string
+    id: string
+    status: BuildStatus,
+    buildUrl: string,
+    startedAt: string,
+    finishedAt: string,
+    jobName: string,
+    jobId: string,
+}
+
+export function circleWorkflowtoStages(workflow: CircleWorkflow): WorkflowStage[] {
+
+    const doc = yaml.load(workflow.config);
+    const jobsConfig = _.find(_.values(doc.workflows), v => v.jobs).jobs;
+    const stages: Stage[] = [];
+    jobsConfig.forEach(jc => {
+        const name = _.head(_.keys(jc));
+        if (jc[name].requires) {
+            let stage =_.find(stages, j => _.isEqual(j.require, jc[name].requires));
+            if (!stage) {
+                stage = {
+                    jobs: [name],
+                    require: jc[name].requires
+                }
+                stages.push(stage);
+            } else {
+                stage.jobs.push(name);
+            }
+        }
+    })
+
+    const orderedStages: Stage[] = [{
+        jobs: ["build"],
+        require: []
+    }];
+    const visitedJobs = ["build"];
+
+    while (orderedStages.length <= stages.length) {
+        const remainingStages: Stage[] = _.clone(stages);
+        _.remove(remainingStages, s => _.includes(orderedStages, s));
+        const stagesRequireOnlyVisitedJobs: Stage[] = remainingStages.filter(s => _.every(s.require, r => _.includes(visitedJobs, r)));
+        const newlyVisitedJobs: string[] = _.uniq(_.flatMap(stagesRequireOnlyVisitedJobs, s => s.jobs));
+        newlyVisitedJobs.forEach(j => visitedJobs.push(j));
+        stagesRequireOnlyVisitedJobs.forEach(s => orderedStages.push(s));
+    }
+
+    const workflowStages: WorkflowStage[] = _.map(orderedStages,s => {
+        const buildsInStage = workflow.builds.filter(b => _.includes(s.jobs, b.jobName))
+        let jobName = s.jobs[0];
+        if (buildsInStage.length == 0) {
+            return { name: jobName } as WorkflowStage
+        }
+        let longestJobDuration = 0;
+        let earliestStart = undefined;
+        let latestEnd = 0;
+        let isFailure = false;
+        buildsInStage.forEach(b => {
+            const startedAt = Date.parse(b.startedAt);
+            const finishedAt = Date.parse(b.finishedAt);
+            earliestStart = _.min([earliestStart, startedAt]);
+            latestEnd = _.max([latestEnd, finishedAt]);
+            const jobDuration = finishedAt - startedAt;
+            if (b.status === "failed") {
+                isFailure = true;
+                jobName = b.jobName;
+            }
+            if (longestJobDuration < jobDuration) {
+                longestJobDuration = jobDuration;
+                if (!isFailure) {
+                    jobName = b.jobName;
+                }
+            }
+        })
+        return {
+            name: jobName,
+            completed: {
+                status: isFailure ? "failed" : "passed",
+                totalDuration: latestEnd - earliestStart,
+                longestJobDuration
+            }
+        } as WorkflowStage
+    })
+    // add times and statuses based on build events
+    return workflowStages;
+}
+
+interface Stage {
+    jobs: string[]
+    require: string[]
+}
---
+++ src/handlers/event/build/workflow/WorkflowStage.ts
@@ -0,0 +1,12 @@
+import {BuildStatus} from "../../../../typings/types";
+
+export interface WorkflowStage {
+    name: string
+    completed?: WorkflowStageCompleted
+}
+
+export interface WorkflowStageCompleted {
+    status: BuildStatus
+    totalDuration: number
+    longestJobDuration: number
+}
