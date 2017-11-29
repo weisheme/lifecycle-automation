b734a2eec3ebfebcc560771ba11489d10596ea22
Christian Dupuis <cd@atomist.com>
2017-11-29 20:24:24 +0100

    create metrics for graphql queries and mutations

--- src/util/datadog.ts
+++ src/util/datadog.ts
@@ -3,9 +3,9 @@ import {
     HandlerContext,
     HandlerResult,
 } from "@atomist/automation-client";
+import { AutomationContextAware } from "@atomist/automation-client/HandlerContext";
 import { CommandInvocation } from "@atomist/automation-client/internal/invoker/Payload";
 import { RequestProcessor } from "@atomist/automation-client/internal/transport/RequestProcessor";
-import * as nsp from "@atomist/automation-client/internal/util/cls";
 import { logger } from "@atomist/automation-client/internal/util/logger";
 import { registerShutdownHook } from "@atomist/automation-client/internal/util/shutdown";
 import { AutomationEventListenerSupport } from "@atomist/automation-client/server/AutomationEventListener";
@@ -39,14 +39,64 @@ export class DatadogAutomationEventListener extends AutomationEventListenerSuppo
         this.event("event.registration", `New registration for ${pj.name}/${pj.version}`);
     }
 
+    public contextCreated(ctx: HandlerContext) {
+        const context = (ctx as any as AutomationContextAware).context;
+        const graphClient = ctx.graphClient;
+
+        // On the cluster master we don't have a GraphClient
+        if (graphClient) {
+
+            const tags = [
+                `atomist_operation:${context.operation}`,
+                `atomist_operation_type:command`,
+                ...this.teamDetail(ctx),
+            ];
+
+            ctx.graphClient = {
+                endpoint: graphClient.endpoint,
+                executeMutation: (mutation: string, variables?: any, options?: any) => {
+                    const start = Date.now();
+                    return graphClient.executeMutation(mutation, variables, options)
+                        .then(result => {
+                            this.statsd.increment("counter.graphql.mutation.success", 1, 1, tags, callback);
+                            this.statsd.timing("timer.graphql.mutation", Date.now() - start, 1, tags, callback);
+                            return result;
+                        })
+                        .catch(err => {
+                            this.statsd.increment("counter.graphql.mutation.failure", 1, 1, tags, callback);
+                            this.statsd.timing("timer.graphql.mutation", Date.now() - start, 1, tags, callback);
+                            return err;
+                        });
+                },
+                executeMutationFromFile: graphClient.executeMutationFromFile,
+                executeQuery: (query: string, variables?: any, options?: any) => {
+                    const start = Date.now();
+                    return graphClient.executeQuery(query, variables, options)
+                        .then(result => {
+                            this.statsd.increment("counter.graphql.query.success", 1, 1, tags, callback);
+                            this.statsd.timing("timer.graphql.query", Date.now() - start, 1, tags, callback);
+                            return result;
+                        })
+                        .catch(err => {
+                            this.statsd.increment("counter.graphql.query.failure", 1, 1, tags, callback);
+                            this.statsd.timing("timer.graphql.query", Date.now() - start, 1, tags, callback);
+                            return err;
+                        });
+
+                },
+                executeQueryFromFile: graphClient.executeQueryFromFile,
+            };
+        }
+    }
+
     public commandSuccessful(payload: CommandInvocation, ctx: HandlerContext, result: HandlerResult): Promise<any> {
         const tags = [
             `atomist_operation:${payload.name}`,
             `atomist_operation_type:command`,
-            ...this.teamDetail(),
+            ...this.teamDetail(ctx),
         ];
         this.increment("counter.operation.success", tags);
-        this.timing("timer.operation", tags);
+        this.timing("timer.operation", ctx, tags);
         return Promise.resolve();
     }
 
@@ -54,10 +104,10 @@ export class DatadogAutomationEventListener extends AutomationEventListenerSuppo
         const tags = [
             `atomist_operation:${payload.name}`,
             `atomist_operation_type:command`,
-            ...this.teamDetail(),
+            ...this.teamDetail(ctx),
         ];
         this.increment("counter.operation.failure", tags );
-        this.timing("timer.operation", tags);
+        this.timing("timer.operation", ctx, tags);
         this.event("event.operation.failure", "Unsuccessfully invoked command", tags);
         return Promise.resolve();
     }
@@ -66,10 +116,10 @@ export class DatadogAutomationEventListener extends AutomationEventListenerSuppo
         const tags = [
             `atomist_operation:${payload.extensions.operationName}`,
             `atomist_operation_type:event`,
-            ...this.teamDetail(),
+            ...this.teamDetail(ctx),
         ];
         this.increment("counter.operation.success", tags);
-        this.timing("timer.operation", tags);
+        this.timing("timer.operation", ctx, tags);
         return Promise.resolve();
     }
 
@@ -77,10 +127,10 @@ export class DatadogAutomationEventListener extends AutomationEventListenerSuppo
         const tags = [
             `atomist_operation:${payload.extensions.operationName}`,
             `atomist_operation_type:event`,
-            ...this.teamDetail(),
+            ...this.teamDetail(ctx),
         ];
         this.increment("counter.operation.failure", tags);
-        this.timing("timer.operation", tags);
+        this.timing("timer.operation", ctx, tags);
         this.event("event.operation.failure", "Unsuccessfully invoked event", tags);
         return Promise.resolve();
     }
@@ -88,7 +138,8 @@ export class DatadogAutomationEventListener extends AutomationEventListenerSuppo
     public messageSent(message: string | SlackMessage,
                        userNames: string | string[],
                        channelName: string | string[],
-                       options?: MessageOptions) {
+                       options: MessageOptions,
+                       ctx: HandlerContext & AutomationContextAware) {
         let type;
         if (userNames && userNames.length > 0) {
             type = "dm";
@@ -99,7 +150,7 @@ export class DatadogAutomationEventListener extends AutomationEventListenerSuppo
         }
         this.increment("counter.message", [
             `atomist_message_type:${type}`,
-            ...this.teamDetail(),
+            ...this.teamDetail(ctx),
         ]);
     }
 
@@ -117,9 +168,14 @@ export class DatadogAutomationEventListener extends AutomationEventListenerSuppo
     }
 
     private timing(stat: string | string[],
+                   ctx: HandlerContext,
                    tags?: string[]) {
-        if (nsp.get().ts && cluster.isMaster) {
-            this.statsd.timing(stat, Date.now() - nsp.get().ts, 1, tags, callback);
+        if (cluster.isMaster &&
+            ctx &&
+            (ctx as any as AutomationContextAware).context &&
+            (ctx as any as AutomationContextAware).context.ts) {
+            const context = (ctx as any as AutomationContextAware).context;
+            this.statsd.timing(stat, Date.now() - context.ts, 1, tags, callback);
         }
     }
 
@@ -139,7 +195,7 @@ export class DatadogAutomationEventListener extends AutomationEventListenerSuppo
         this.statsd = new StatsD(options);
         this.timer = setInterval(() => {
             this.submitHeapStats();
-        }, 1000);
+        }, 5000);
 
         // Register orderly shutdown
         registerShutdownHook(() => {
@@ -151,13 +207,17 @@ export class DatadogAutomationEventListener extends AutomationEventListenerSuppo
         });
     }
 
-    private teamDetail(): string[] {
-        const session = nsp.get();
-        return [
-            `atomist_team_id:${session.teamId}`,
-            `atomist_team_name:${session.teamName ? stripchar.RSExceptUnsAlpNum(session.teamName)
-                .trim().replace(/ /g, "_") : undefined}`,
-        ];
+    private teamDetail(ctx: HandlerContext): string[] {
+        if (ctx && (ctx as any as AutomationContextAware).context) {
+            const context = (ctx as any as AutomationContextAware).context;
+            return [
+                `atomist_team_id:${context.teamId}`,
+                `atomist_team_name:${context.teamName ? stripchar.RSExceptUnsAlpNum(context.teamName)
+                    .trim().replace(/ /g, "_") : undefined}`,
+            ];
+        } else {
+            return [];
+        }
     }
 
     private submitHeapStats() {
