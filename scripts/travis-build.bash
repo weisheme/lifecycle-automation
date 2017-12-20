22d612b04ec8e88c6ee870f993004491bce8fef6
David Dooling <david@atomist.com>
2017-12-20 11:12:10 -0600

    Update Travis build, publish TypeDoc
    Update Travis build script to latest version.  Update package scripts.
    Remove Cloud Foundry environment variables.  Add TypeDoc dependency,
    generation, and publish to gh-pages.  Move copyfiles to
    devDependencies.
    
    The suggestedReviewers.graphql failed to generate proper types, so src
    was excluded from type generation.  This is not really a change since
    it was not included before, just noting.

--- scripts/travis-build.bash
+++ scripts/travis-build.bash
@@ -1,10 +1,10 @@
 #!/bin/bash
-# build and test a node module
+# build and test a node package
 
 set -o pipefail
 
-declare Pkg=travis-build-node-docker
-declare Version=0.2.0
+declare Pkg=travis-build-node
+declare Version=1.1.1
 
 # write message to standard out (stdout)
 # usage: msg MESSAGE
@@ -67,7 +67,7 @@ function set-timestamp-version () {
     fi
 
     local pkg_version pkg_json=package.json
-    pkg_version=$(jq -e --raw-output .version "$pkg_json")
+    pkg_version=$(jq -er .version "$pkg_json")
     if [[ $? -ne 0 || ! $pkg_version ]]; then
         err "failed to parse version from $pkg_json"
         return 1
@@ -79,7 +79,7 @@ function set-timestamp-version () {
         return 1
     fi
     local project_version=$pkg_version-$prerelease$timestamp
-    if ! npm version "$project_version"; then
+    if ! npm version --git-tag-version false "$project_version"; then
         err "failed to set package version: $project_version"
         return 1
     fi
@@ -88,7 +88,6 @@ function set-timestamp-version () {
 # npm publish
 # usage: npm-publish [NPM_PUBLISH_ARGS]...
 function npm-publish () {
-    msg "packaging module"
     if ! cp -r build/src/* .; then
         err "packaging module failed"
         return 1
@@ -98,7 +97,7 @@ function npm-publish () {
     rm -f .gitignore
 
     if ! npm publish "$@"; then
-        err "failed to publish node module"
+        err "failed to publish node package"
         cat "$(ls -t "$HOME"/.npm/_logs/*-debug.log | head -n 1)"
         return 1
     fi
@@ -118,8 +117,8 @@ function npm-publish () {
 # publish a public prerelease version to non-standard registry
 # usage: npm-publish-prerelease [BRANCH]
 function npm-publish-prerelease () {
-    local package_version=$1
-    if [[ ! $package_version ]]; then
+    local pkg_version=$1
+    if [[ ! $pkg_version ]]; then
         err "npm-publish-prerelease: missing required argument: PACKAGE_VERSION"
         return 10
     fi
@@ -127,12 +126,12 @@ function npm-publish-prerelease () {
 
     if [[ ! $NPM_REGISTRY ]]; then
         msg "no team NPM registry set"
+        msg "skipping prerelease package publication"
         return 0
     fi
 
-    msg "publishing NPM module version $package_version"
     if ! npm-publish --registry "$NPM_REGISTRY" --access public; then
-        err "failed to publish to Artifactory NPM registry"
+        err "failed to publish to NPM registry '$NPM_REGISTRY'"
         return 1
     fi
 
@@ -143,16 +142,16 @@ function npm-publish-prerelease () {
         sha=$TRAVIS_COMMIT
     fi
 
-    local module_name pkg_json=package.json
-    module_name=$(jq -er .name "$pkg_json")
-    if [[ $? -ne 0 || ! $module_name ]]; then
-        err "failed to parse NPM module name from $pkg_json"
+    local pkg_name pkg_json=package.json
+    pkg_name=$(jq -er .name "$pkg_json")
+    if [[ $? -ne 0 || ! $pkg_name ]]; then
+        err "failed to parse NPM package name from '$pkg_json'"
         return 1
     fi
-    local module_url=https://atomist.jfrog.io/atomist/npm-dev/$module_name/-/$module_name-$package_version.tgz
+    local pkg_url=https://atomist.jfrog.io/atomist/npm-dev/$pkg_name/-/$pkg_name-$pkg_version.tgz
     local status_url=https://api.github.com/repos/$TRAVIS_REPO_SLUG/statuses/$sha
     local post_data
-    printf -v post_data '{"state":"success","target_url":"%s","description":"Pre-release NPM module publication","context":"npm/atomist/prerelease"}' "$module_url"
+    printf -v post_data '{"state":"success","target_url":"%s","description":"Pre-release NPM module publication","context":"npm/atomist/prerelease"}' "$pkg_url"
     if ! curl -s -H 'Accept: application/vnd.github.v3+json' \
             -H 'Content-Type: application/json' \
             -H "Authorization: token $GITHUB_TOKEN" \
@@ -161,27 +160,37 @@ function npm-publish-prerelease () {
         err "failed to post status on commit: $sha"
         return 1
     fi
-    msg "posted module URL $module_url to commit status $status_url"
+    msg "posted module URL '$pkg_url' to commit status '$status_url'"
 }
 
 # create and push a Docker image
-# usage: docker-push VERSION
+# usage: docker-push IMAGE VERSION
 function docker-push () {
+    local image_name=$1
+    if [[ ! $image_name ]]; then
+        err "docker-push: missing required argument: NAME"
+        return 10
+    fi
+    shift
     local image_version=$1
     if [[ ! $image_version ]]; then
         err "docker-push: missing required argument: VERSION"
         return 10
     fi
+    shift
 
-    local repo_name=${TRAVIS_REPO_SLUG##*/}
+    if [[ ! $DOCKER_REGISTRY ]]; then
+        msg "no Docker registry set"
+        msg "skipping Docker build and push"
+        return 0
+    fi
 
-    local registry=sforzando-dockerv2-local.jfrog.io
-    if ! docker login -u "$DOCKER_USER" -p "$DOCKER_PASSWORD" "$registry"; then
-        err "failed to login to docker registry: $registry"
+    if ! docker login -u "$DOCKER_USER" -p "$DOCKER_PASSWORD" "$DOCKER_REGISTRY"; then
+        err "failed to login to docker registry: $DOCKER_REGISTRY"
         return 1
     fi
 
-    local tag=$registry/$repo_name:$image_version
+    local tag=$DOCKER_REGISTRY/$image_name:$image_version
     if ! docker build . -t "$tag"; then
         err "failed to build docker image: '$tag'"
         return 1
@@ -192,7 +201,41 @@ function docker-push () {
         return 1
     fi
 
-    msg "built and pushed docker image"
+    msg "built and pushed Docker image"
+}
+
+# push app to Cloud Foundry
+# usage: cf-push APP [SPACE]
+function cf-push () {
+    local app=$1
+    if [[ ! $app ]]; then
+        err "cf-push: missing required argument: APPLICATION"
+        return 10
+    fi
+    shift
+    local space=$1
+    if [[ ! $space ]]; then
+        space=production
+    fi
+
+    if [[ ! $CF_ORG ]]; then
+        msg "no Cloud Foundry org set"
+        msg "skipping Cloud Foundry push"
+        return 0
+    fi
+
+    local api_url=${CF_API:-https://api.run.pivotal.io}
+
+    if ! cf login -a "$api_url" -u "$CF_USER" -p "$CF_PASSWORD" -o "$CF_ORG" -s "$space"
+    then
+        err "failed to log in to Cloud Foundry at '$api_url'"
+        return 1
+    fi
+
+    if ! cf push "$app"; then
+        err "failed to push '$app' to Cloud Foundry"
+        return 1
+    fi
 }
 
 # usage: main "$@"
@@ -209,6 +252,18 @@ function main () {
                 ;;
         esac
     done
+
+    msg "running compile"
+    if ! npm run compile; then
+        err "compilation failed"
+        return 1
+    fi
+
+    msg "running tests"
+    if ! npm test; then
+        err "test failed"
+        return 1
+    fi
 
     msg "running lint"
     local lint_status
@@ -228,23 +283,24 @@ function main () {
         return 1
     fi
 
-    msg "compiling TypeScript"
-    if ! npm run compile; then
-        err "compilation failed"
-        return 1
-    fi
-
-    msg "running tests"
-    if ! npm test; then
-        err "test failed"
+    msg "running typedoc"
+    if ! npm run typedoc; then
+        err "failed to generate TypeDoc"
         return 1
     fi
 
     [[ $TRAVIS_PULL_REQUEST == false ]] || return 0
 
+    local app=${TRAVIS_REPO_SLUG##*/}
     if [[ $TRAVIS_TAG =~ ^[0-9]+\.[0-9]+\.[0-9]+(-(m|rc)\.[0-9]+)?$ ]]; then
+        msg "publishing NPM package version '$TRAVIS_TAG'"
         if ! npm-publish --access public; then
             err "failed to publish tag build: '$TRAVIS_TAG'"
+            return 1
+        fi
+        msg "pushing app to Cloud Foundry"
+        if ! cf-push "$app"; then
+            err "failed to push '$app' to Cloud Foundry"
             return 1
         fi
         if ! git-tag "$TRAVIS_TAG+travis.$TRAVIS_BUILD_NUMBER"; then
@@ -256,23 +312,29 @@ function main () {
             return 1
         fi
         local prerelease_version pkg_json=package.json
-        prerelease_version=$(jq -e --raw-output .version "$pkg_json")
+        prerelease_version=$(jq -er .version "$pkg_json")
         if [[ $? -ne 0 || ! $prerelease_version ]]; then
             err "failed to parse version from $pkg_json: $prerelease_version"
             return 1
         fi
+        msg "publishing NPM package version '$prerelease_version'"
         if ! npm-publish-prerelease "$prerelease_version"; then
             err "failed to publish version '$prerelease_version'"
             return 1
         fi
-
         if [[ $TRAVIS_BRANCH == master ]]; then
-            if ! docker-push "$prerelease_version"; then
+            msg "building and pushing Docker image"
+            if ! docker-push "$app" "$prerelease_version"; then
                 err "failed to build and push docker image"
                 return 1
             fi
+            local staging_app=$app-staging
+            msg "pushing staging app to Cloud Foundry development space"
+            if ! cf-push "$staging_app" development; then
+                err "failed to push '$staging_app' to Cloud Foundry"
+                return 1
+            fi
         fi
-
         if ! git-tag "$prerelease_version" "$prerelease_version+travis.$TRAVIS_BUILD_NUMBER"; then
             return 1
         fi
