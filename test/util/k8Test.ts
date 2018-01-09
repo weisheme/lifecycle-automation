a2649f3f6b0449e58621107044da366a905244ae
Clay McCoy <clay@atomist.com>
2018-01-08 22:44:13 -0600

    aggregate the containers running a specified image across pods

---
+++ test/util/k8Test.ts
@@ -0,0 +1,169 @@
+import "mocha";
+import * as assert from "power-assert";
+import {Container, ContainersForPod, Pod, PodsForPhase, podStatusForImage} from "../../src/util/k8";
+
+describe("k8", () => {
+
+    const singleStartedPod = [{
+        name: "pod1",
+        phase: "Started",
+        containers: [
+            {
+                name: "c1",
+                imageName: "i1",
+            } as Container,
+        ],
+    } as Pod];
+
+    const assortedPods = [
+        {
+            name: "pod1",
+            phase: "Started",
+            containers: [
+                {
+                    name: "c1",
+                    imageName: "i1",
+                } as Container,
+            ],
+        } as Pod,
+        {
+            name: "pod2",
+            phase: "Started",
+            containers: [
+                {
+                    name: "c2",
+                    imageName: "i2",
+                } as Container,
+            ],
+        } as Pod,
+        {
+            name: "pod3",
+            phase: "Pulling",
+            containers: [
+                {
+                    name: "c31",
+                    imageName: "i1",
+                } as Container,
+                {
+                    name: "c32",
+                    imageName: "i2",
+                } as Container,
+                {
+                    name: "c33",
+                    imageName: "i1",
+                } as Container,
+            ],
+        } as Pod,
+        {
+            name: "pod4",
+            phase: "Started",
+            containers: [
+                {
+                    name: "c4",
+                    imageName: "i2",
+                } as Container,
+            ],
+        } as Pod,
+    ];
+
+    describe("podStatusForImage", () => {
+
+        it("should return empty array for empty pods", () => {
+            const result = podStatusForImage([], "123");
+            assert.deepEqual(result, []);
+        });
+
+        it("should return empty array for missing image name", () => {
+            const result = podStatusForImage(singleStartedPod, undefined);
+            assert.deepEqual(result, []);
+        });
+
+        it("should return empty array for non matching image name", () => {
+            const result = podStatusForImage(singleStartedPod, "i2");
+            assert.deepEqual(result, []);
+        });
+
+        it("should return container matching image name", () => {
+            const result = podStatusForImage(singleStartedPod, "i1");
+            assert.deepEqual(result, [
+                {
+                    phase: "Started",
+                    pods: [{
+                        podName: "pod1",
+                        containers: [{
+                            name: "c1",
+                            imageName: "i1",
+                        } as Container],
+                    } as ContainersForPod],
+                } as PodsForPhase,
+            ]);
+        });
+
+        it("should return containers across pods for matching image name", () => {
+            const result = podStatusForImage(assortedPods, "i1");
+            assert.deepEqual(result, [
+                {
+                    phase: "Started",
+                    pods: [{
+                        podName: "pod1",
+                        containers: [{
+                            name: "c1",
+                            imageName: "i1",
+                        } as Container],
+                    } as ContainersForPod],
+                } as PodsForPhase, {
+                    phase: "Pulling",
+                    pods: [{
+                        podName: "pod3",
+                        containers: [
+                            {
+                                name: "c31",
+                                imageName: "i1",
+                            } as Container,
+                            {
+                                name: "c33",
+                                imageName: "i1",
+                            } as Container,
+                        ],
+                    } as ContainersForPod],
+                },
+            ]);
+        });
+
+        it("should return containers across pods with phase collision", () => {
+            const result = podStatusForImage(assortedPods, "i2");
+            assert.deepEqual(result, [
+                {
+                    phase: "Started",
+                    pods: [
+                        {
+                            podName: "pod2",
+                            containers: [{
+                                name: "c2",
+                                imageName: "i2",
+                            } as Container],
+                        } as ContainersForPod,
+                        {
+                            podName: "pod4",
+                            containers: [{
+                                name: "c4",
+                                imageName: "i2",
+                            } as Container],
+                        } as ContainersForPod,
+                    ],
+                } as PodsForPhase, {
+                    phase: "Pulling",
+                    pods: [{
+                        podName: "pod3",
+                        containers: [{
+                            name: "c32",
+                            imageName: "i2",
+                        } as Container],
+                    } as ContainersForPod],
+                },
+            ]);
+        });
+
+    });
+
+});
