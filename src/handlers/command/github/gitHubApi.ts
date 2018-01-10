3b1ac5ded988fc9d14a2ba3b27d0c35e53f0661e
Christian Dupuis <cd@atomist.com>
2018-01-10 09:24:06 +0100

    fix up configuration and update deps

--- src/handlers/command/github/gitHubApi.ts
+++ src/handlers/command/github/gitHubApi.ts
@@ -14,11 +14,9 @@ export function api(token: string, apiUrl: string = "https://api.github.com/"): 
     const url = URL.parse(apiUrl);
 
     const gitHubApi = new GitHubApi({
-        debug: false,
         host: url.hostname,
         protocol: url.protocol.slice(0, -1),
         port: +url.port,
-        followRedirects: false,
     });
 
     gitHubApi.authenticate({ type: "token", token });
