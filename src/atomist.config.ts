4bbbb2b31a6682c51986af009343e9af78900109
Christian Dupuis <cd@atomist.com>
2018-01-10 19:06:11 +0100

    remove intercom support

--- src/atomist.config.ts
+++ src/atomist.config.ts
@@ -88,7 +88,6 @@ import { TagToPushLifecycle } from "./handlers/event/push/TagToPushLifecycle";
 import { NotifyAuthorOnReview } from "./handlers/event/review/NotifyAuthorOnReview";
 import { GitHubWebhookCreated } from "./handlers/event/webhook/GitHubWebhookCreated";
 import { DatadogAutomationEventListener, DatadogOptions } from "./util/datadog";
-import { IntercomAutomationEventListener } from "./util/intercom";
 import {
     LogzioAutomationEventListener,
     LogzioOptions,
@@ -124,10 +123,6 @@ const listeners = [
 // Logz.io will only work in certain environments
 if (logzioOptions.token) {
     listeners.push(new LogzioAutomationEventListener(logzioOptions));
-}
-
-if (secret("intercom.token")) {
-    listeners.push(new IntercomAutomationEventListener(secret("intercom.token")));
 }
 
 // StatsD/Datadog servers aren't available locally either
