807e5cc35862154aa73702435168172b65da0867
Christian Dupuis <cd@atomist.com>
2018-01-11 23:18:37 +0100

    fix handling of special empty named release

--- src/handlers/event/push/rendering/PushNodeRenderers.ts
+++ src/handlers/event/push/rendering/PushNodeRenderers.ts
@@ -423,7 +423,9 @@ export class TagNodeRenderer extends AbstractIdentifiableContribution
         }
         // Add the release to the message
         if (tag.release) {
-            if (tag.release.name !== tag.name) {
+            if (tag.release.name === "") {
+                message = `${message} | ${url(tagUrl(repo, tag), "Release")}`;
+            } else if (tag.release.name !== tag.name) {
                 message = `${message} | ${url(tagUrl(repo, tag.release), `Release ${codeLine(tag.release.name)}`)}`;
             } else {
                 message = `${message} | ${url(tagUrl(repo, tag.release), "Release")}`;
